3e7912bc53bb1cb1ac6c5f63c7bba9bf8a2a2826
kvk1999 <koushikraghavan@gmail.com>
2026-08-15 23:03:13 +0530

    Initial commit: pok

--- client/src/api/rectangleApi.js
+++ client/src/api/rectangleApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://ecommerceshop-hgbi.onrender.com/api";
+const BASE_URL = "http://localhost:5000/api";
 
 export const calculateRectangle = async (length, width) => {
   const res = await fetch(`${BASE_URL}/calculate`, {
